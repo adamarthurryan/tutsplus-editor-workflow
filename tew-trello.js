c5119bcd73523856558675f23befe0dfdf521d1b
Adam Brown <adamarthurryan@gmail.com>
2018-12-18 23:18:21 -0500

    update trello workflow

--- tew-trello.js
+++ tew-trello.js
@@ -1,6 +1,7 @@
 // Ingest the scraped trello board data and format it for copying to code editorial calendar.
 
 const Papa = require("papaparse")
+const dateFormat =require('dateformat')
 
 const options = {
 	dynamicTyping: true,
@@ -42,15 +43,38 @@ function processRow (row)  {
 		labels = labels.map(label => label['card-labels-title'])
 	}
 
+	if (date && date != "null") {
+		date = date + " " + (new Date()).getUTCFullYear()
+		date = dateFormat(Date.parse(date), "yyyy-mm-dd")
+	}
+
 	let card = {list, title, date, authors, labels}
+
 	cards.push(card)
 }
 
-const LISTS = ["In Copy Editing", "Ready For Review", "In Review", "In Progress", "Scheduled For Publication"].map(str => str.toLowerCase())
+const LISTS = ["In Progress","Ready For Review", "In Review","In Copy Editing","Scheduled For Publication"].map(str => str.toLowerCase())
 
 function processEnd () {
 	cards = cards.filter( ({list}) => LISTS.includes(list.toLowerCase()))
 
-	const outputCSV = Papa.unparse(cards)
+	//sort cards by date (secondary) and status (primary)
+
+	cards.map(card => Object.assign(card, {date: (! card.date || card.date == "null") ? "9999-12-31" : card.date }))
+	cards = cards.sort( (a, b) => 
+		10000*(LISTS.indexOf(b.list.toLowerCase()) - LISTS.indexOf(a.list.toLowerCase())) 
+			+ a.date.localeCompare(b.date))
+//	cards = cards.sort( (a, b) => ))
+	cards.map(card => Object.assign(card, {date: (card.date == "9999-12-31") ? "" : card.date }))
+
+
+	const calendarLines = cards.map(card => formatForEdCal(card))
+	const outputCSV = Papa.unparse(calendarLines)
+
+
 	console.log(outputCSV)
+}
+
+function formatForEdCal({list, title, date, authors, labels}) {
+	return {date, day:"", goal:"", title, authors:authors, rate_code:"", rate_cm:"", rate_sponsored:"", status:list, content_strategy:labels, invoice:""}
 }
